b5e54a3238d3ededebbd3f4301e42194f620d1a6
JulienRobberechts <jrobberechts@findmypast.com>
2019-09-18 22:40:45 +0100

    comments on testdouble-v2

--- testdouble-with-interception-testdouble-v2/test/car-error.spec.js
+++ testdouble-with-interception-testdouble-v2/test/car-error.spec.js
@@ -2,33 +2,31 @@ var td = require("testdouble");
 var chai = require("chai");
 var expect = chai.expect;
 
+// you can't declare the system under test before replacing the dependencies:
+var car = require("../lib/car");
+
 var gasPedal, accelerometer;
-var car;
 
 describe("goSixty", function() {
   beforeEach(function() {
     gasPedal = td.replace("../lib/gas-pedal"); // <-- a plain ol' function
     accelerometer = td.replace("../lib/accelerometer"); // <-- an obj of functions
-    car = require("../lib/car");
+
+    // The correct way would be to call require after replacing the dependencies:
+    // car = require("../lib/car");
   });
 
   it("not yet going 60 -> pushes the pedal down 5 units", function() {
-    // Arrange
     td.when(accelerometer.read()).thenReturn(55);
 
-    // Act
-    const actual = car.goSixty();
-
-    // Assert 1: dependency accelerometer has been called
-    td.verify(accelerometer.read());
-
-    // Assert 2: dependency gasPedal has been called with param 5
-    const expectedGasPedalParameter = 5;
-    td.verify(gasPedal(expectedGasPedalParameter));
-
-    // Assert 3: result is "OK"
-    const expectedResult = "OK";
-    expect(actual).to.equal(expectedResult);
+    try {
+      const actual = car.goSixty();
+    } catch (error) {
+      console.log(
+        "Expected unimplemented error because the system under test was declared before the replacing the dependencies:",
+        error.message
+      );
+    }
   });
   afterEach(function() {
     td.reset();
