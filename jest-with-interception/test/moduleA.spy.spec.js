6cc777ba396274d4bee500ed330c3c8ecab1dd7c
JulienRobberechts <jrobberechts@findmypast.com>
2019-09-18 14:18:13 +0100

    spy with sinon and proxyquire

--- jest-with-interception/test/moduleA.spy.spec.js
+++ jest-with-interception/test/moduleA.spy.spec.js
@@ -10,6 +10,12 @@ beforeEach(() => jest.clearAllMocks());
 // in fact the behavior is modified it's not really a spy.
 // when there is interception, it's not a spy??
 test("moduleB can't be spied without changing the behaviour Jest without implementation", () => {
-  expect(moduleA.DoItA()).toEqual("A(undefined)");
+  const expected = "A(undefined)";
+  const actual = moduleA.DoItA();
+
   expect(moduleBMocked.DoItB).toHaveBeenCalled();
+
+  // In fact the behavior is modified it's not really a spy,
+  // just a mock with, it's just a stub with empty behavior.
+  expect(actual).toEqual(expected);
 });
--- sinon-with-interception-proxyquire/test/moduleA.spy.spec.js
+++ sinon-with-interception-proxyquire/test/moduleA.spy.spec.js
@@ -3,10 +3,9 @@ var chai = require("chai");
 var proxyquire = require("proxyquire").noPreserveCache();
 var expect = chai.expect;
 
-describe("moduleB intercepted by proxyquire and stub by Sinon", function() {
-  it("should return A(beta)", function() {
+describe("moduleB intercepted by proxyquire and spied by Sinon (but with empty behavior)", function() {
+  it("should return A(B)", function() {
     let moduleBStub = sinon.stub();
-    moduleBStub.returns("beta");
 
     // import the module to test, using a fake dependency
     const moduleA = proxyquire("../lib/moduleA", {
@@ -15,11 +14,13 @@ describe("moduleB intercepted by proxyquire and stub by Sinon", function() {
       }
     });
 
-    const expected = "A(beta)";
+    const expected = "A(undefined)";
     const actual = moduleA.DoItA();
 
     expect(moduleBStub.called).to.be.true;
 
+    // In fact the behavior is modified it's not really a spy,
+    // just a mock with, it's just a stub with empty behavior.
     expect(actual).to.be.equal(expected);
   });
 });
