c6032904dee8c84336f3b04fb3e30a6019f767c4
JulienRobberechts <jrobberechts@findmypast.com>
2019-09-18 18:26:39 +0100

    clean testdouble-with-interception-testdouble-v2

--- testdouble-with-interception-testdouble-v2/lib/car.js
+++ testdouble-with-interception-testdouble-v2/lib/car.js
@@ -1,19 +1,10 @@
-var accelerometer = require('./accelerometer')
-var gasPedal = require('./gas-pedal')
-var Brake = require('./brake')
-var copilot = require('./copilot')
+var accelerometer = require("./accelerometer");
+var gasPedal = require("./gas-pedal");
 
 module.exports = {
-  goSixty: function () {
-    var speed = accelerometer.read()
-    var brake = new Brake()
-
-    if (speed < 60) {
-      gasPedal(60 - speed)
-    } else if (speed > 60) {
-      brake.engage(speed - 60)
-    } else {
-      return copilot()
-    }
+  goSixty: function() {
+    var speed = accelerometer.read();
+    gasPedal(60 - speed);
+    return "OK";
   }
-}
+};
--- testdouble-with-interception-testdouble-v2/test/lib/car.spec.js
+++ testdouble-with-interception-testdouble-v2/test/lib/car.spec.js
@@ -2,26 +2,26 @@ var td = require("testdouble");
 var chai = require("chai");
 var expect = chai.expect;
 
-var subject, gasPedal, accelerometer, Brake;
+var car, gasPedal, accelerometer;
 
 describe("moduleB.DoItB intercepted and stub by testdouble", function() {
   beforeEach(function() {
     gasPedal = td.replace("../../lib/gas-pedal"); // <-- a plain ol' function
     accelerometer = td.replace("../../lib/accelerometer"); // <-- an obj of functions
-    Brake = td.replace("../../lib/brake"); // <-- a constructor function
-    td.replace("../../lib/copilot", function() {
-      return "HIGHFIVE";
-    }); // <-- a manual override
-    subject = require("../../lib/car");
+    car = require("../../lib/car");
   });
 
   describe("goSixty", function() {
     it("not yet going 60 -> pushes the pedal down 5 units", function() {
       td.when(accelerometer.read()).thenReturn(55);
 
-      subject.goSixty();
+      const actual = car.goSixty();
 
-      td.verify(gasPedal(5));
+      const expectedResult = "OK";
+      expect(actual).to.equal(expectedResult);
+
+      const expectedGasPedalParameter = 5;
+      td.verify(gasPedal(expectedGasPedalParameter));
     });
   });
 });
