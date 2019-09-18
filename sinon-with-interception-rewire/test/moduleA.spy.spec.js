c801273baaac972972d60e9fad438d773e01c9fc
JulienRobberechts <jrobberechts@findmypast.com>
2019-09-18 15:20:03 +0100

    sinon-with-interception-rewire

--- sinon-with-interception-rewire/test/moduleA.spy.spec.js
+++ sinon-with-interception-rewire/test/moduleA.spy.spec.js
@@ -5,20 +5,24 @@ var expect = chai.expect;
 
 // intercept moduleB.DoItB in myModule with rewire
 var moduleA = rewire("../lib/moduleA");
-let DoItBStub = sinon.stub();
-DoItBStub.returns("beta");
+let DoItBSpy = sinon.stub();
+// DoItBStub.returns("beta");
 
 var moduleBMock = {
-  DoItB: DoItBStub
+  DoItB: DoItBSpy
 };
 
 moduleA.__set__("moduleB", moduleBMock);
 
 describe("moduleB.DoItB intercepted by rewire and stub by Sinon", function() {
-  it("should return A(beta)", function() {
+  it("should return A(undefined)", function() {
     const actual = moduleA.DoItA();
-    const expected = "A(beta)";
-    expect(DoItBStub.called).to.be.true;
+    const expected = "A(undefined)";
+
+    expect(DoItBSpy.called).to.be.true;
+
+    // In fact the behavior is modified it's not really a spy,
+    // just a mock with, it's just a stub with empty behavior.
     expect(actual).to.be.equal(expected);
   });
 });
