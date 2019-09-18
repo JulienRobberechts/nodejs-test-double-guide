88c388c380c81b32e4a99e28adff9958f4c8512f
JulienRobberechts <jrobberechts@findmypast.com>
2019-09-18 15:20:14 +0100

    clean var names

--- sinon-with-interception-proxyquire/test/moduleA.spy.spec.js
+++ sinon-with-interception-proxyquire/test/moduleA.spy.spec.js
@@ -4,20 +4,20 @@ var proxyquire = require("proxyquire").noPreserveCache();
 var expect = chai.expect;
 
 describe("moduleB intercepted by proxyquire and spied by Sinon (but with empty behavior)", function() {
-  it("should return A(B)", function() {
-    let moduleBStub = sinon.stub();
+  it("should return A(undefined)", function() {
+    let DoItBSpy = sinon.stub();
 
     // import the module to test, using a fake dependency
     const moduleA = proxyquire("../lib/moduleA", {
       "./moduleB": {
-        DoItB: moduleBStub
+        DoItB: DoItBSpy
       }
     });
 
     const expected = "A(undefined)";
     const actual = moduleA.DoItA();
 
-    expect(moduleBStub.called).to.be.true;
+    expect(DoItBSpy.called).to.be.true;
 
     // In fact the behavior is modified it's not really a spy,
     // just a mock with, it's just a stub with empty behavior.
