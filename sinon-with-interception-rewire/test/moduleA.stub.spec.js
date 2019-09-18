14e68f9d3c98490bb9b56d64f166e406b44e9398
JulienRobberechts <jrobberechts@findmypast.com>
2019-09-18 15:10:11 +0100

    sinon-with-interception-rewire

--- sinon-with-interception-rewire/test/moduleA.stub.spec.js
+++ sinon-with-interception-rewire/test/moduleA.stub.spec.js
@@ -3,24 +3,20 @@ var sinon = require("sinon");
 var chai = require("chai");
 var expect = chai.expect;
 
-var moduleA = require("../lib/moduleA"); // module to test
-var moduleB = rewire("../lib/moduleB"); // module to stub
-var DoItBStub;
+// intercept moduleB.DoItB in myModule with rewire
+var moduleA = rewire("../lib/moduleA");
+var moduleBMock = {
+  DoItB: function() {
+    return "beta";
+  }
+};
+moduleA.__set__("moduleB", moduleBMock);
 
-describe("moduleB intercepted by proxyquire and stub by Sinon", function() {
-  beforeEach(function() {
-    DoItBStub = sinon.stub(); // create a stub for every test
-    DoItBStub.returns("beta");
-
-    // import the module to test, using a fake dependency
-    moduleB.__set__({
-      DoItB: DoItBStub
-    });
-  });
+describe("moduleB intercepted by rewire and stub by Sinon", function() {
   it("should return A(beta)", function() {
-    const expected = "A(beta)";
     const actual = moduleA.DoItA();
-    expect(DoItBStub.called).to.be.true;
+    const expected = "A(beta)";
+
     expect(actual).to.be.equal(expected);
   });
 });
