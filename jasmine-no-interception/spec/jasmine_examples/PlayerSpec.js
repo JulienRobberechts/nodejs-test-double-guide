299b461d21e8160ac603f6c7b12c6e82daa27e70
JulienRobberechts <omicron4dev@gmail.com>
2019-09-22 20:42:28 +0100

    jasmine tests

--- jasmine-no-interception/spec/jasmine_examples/PlayerSpec.js
+++ jasmine-no-interception/spec/jasmine_examples/PlayerSpec.js
@@ -1,6 +1,8 @@
+var Player = require('../../lib/jasmine_examples/Player');
+var Song = require('../../lib/jasmine_examples/Song');
+
 describe("Player", function() {
-  var Player = require('../../lib/jasmine_examples/Player');
-  var Song = require('../../lib/jasmine_examples/Song');
+
   var player;
   var song;
 
---
+++ jasmine-no-interception/spec/jasmine_examples/sampleSpec.js
@@ -0,0 +1,28 @@
+var moduleA = require('../../lib/jasmine_examples/moduleA');
+var moduleB = require('../../lib/jasmine_examples/moduleB');
+
+describe("module A tests with Jasmine spy", function () {
+  beforeEach(function () {
+    console.log('beforeEach');
+  });
+  it("Jasmine spy delete the behavior of the function: impossible to use it as a spy?", function () {
+    var DoItBSpy = spyOn(moduleB, 'DoItB');
+    const actual = moduleA.DoItA();
+    const expected = 'A(undefined)';
+    expect(true).toBe(true);
+    expect(DoItBSpy).toHaveBeenCalled();
+    expect(actual).toBe(expected);
+  });
+  it("Jasmine stub: ok", function () {
+    var DoItBStub = spyOn(moduleB, 'DoItB').and.returnValue('beta');
+    const actual = moduleA.DoItA();
+    const expected = 'A(beta)';
+    expect(true).toBe(true);
+    expect(DoItBStub).toHaveBeenCalled();
+    expect(actual).toBe(expected);
+  });
+  afterEach(function () {
+    console.log('afterEach');
+  });
+});
+
