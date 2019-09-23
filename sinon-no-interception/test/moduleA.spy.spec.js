cabd0c2d59d330d6233c0cded8cf8bbc10ea0179
JulienRobberechts <omicron4dev@gmail.com>
2019-09-23 05:55:13 +0100

    remove test for moduleA

--- sinon-no-interception/test/moduleA.spy.spec.js
+++ sinon-no-interception/test/moduleA.spy.spec.js
@@ -6,13 +6,6 @@ var chai = require("chai");
 var expect = chai.expect;
 
 describe("module A tests with sinon spy", () => {
-  it("This test should call DoItA - obvious test just for demo", () => {
-    const spy = sinon.spy(moduleA, "DoItA");
-    expect(moduleA.DoItA()).to.be.equal("A(B)");
-    expect(spy.called).to.be.true;
-    sinon.restore();
-  });
-
   it("The implementation of A should call DoItB in module B - interesting test", () => {
     const spy = sinon.spy(moduleB, "DoItB");
     expect(moduleA.DoItA()).to.be.equal("A(B)");
