5284e12ae265680d75a3c29512b9188541e578a2
JulienRobberechts <omicron4dev@gmail.com>
2019-09-23 06:29:57 +0100

    destruct is allowed in the sut

--- sinon-no-interception/test/moduleA.spy.spec.js
+++ sinon-no-interception/test/moduleA.spy.spec.js
@@ -1,14 +1,14 @@
 "use strict";
 const sinon = require("sinon");
-const moduleA = require("../lib/moduleA");
-const moduleB = require("../lib/moduleB");
+const { DoItA } = require("../lib/moduleA"); // you can destructure this dependency
+const moduleB = require("../lib/moduleB"); // you can't destructure this dependency
 var chai = require("chai");
 var expect = chai.expect;
 
 describe("module A tests with sinon spy", () => {
   it("The implementation of A should call DoItB in module B - interesting test", () => {
     const spy = sinon.spy(moduleB, "DoItB");
-    expect(moduleA.DoItA()).to.be.equal("A(B)");
+    expect(DoItA()).to.be.equal("A(B)");
     expect(spy.called).to.be.true;
     sinon.restore();
   });
